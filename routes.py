9d39a394b1931ccff32935753cae635f09954e93
Ayush Patel <me@ayush.info>
2024-08-29 16:39:52 +0530

    set up flask

---
+++ routes.py
@@ -0,0 +1,8 @@
+from flask import render_template, request, redirect, url_for, flash, session
+from app import app
+
+app.config['SECRET_KEY'] = "MY SECRET"
+
+@app.route('/')
+def index():
+    return render_template('index.html')
