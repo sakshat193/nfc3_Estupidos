a896b8b27fc81a9f0de0e1acc6df096466e676b5
sakshat193 <sakshat193@gmail.com>
2024-08-30 09:53:54 +0530

    Update Stock_Management.py

--- pages/Stock_Management.py
+++ pages/Stock_Management.py
@@ -6,9 +6,6 @@ from prophet.plot import plot_plotly
 import pandas as pd
 import os
 
-# Set your OpenAI API key
-os.environ["OPENAI_API_KEY"] = "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"
-
 # Set page title and icon
 st.set_page_config(page_title="Company Stock Data Viewer", page_icon=":moneybag:", layout="wide")
 
