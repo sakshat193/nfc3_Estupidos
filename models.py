a802fdc2056fb01d7c4c70818d688f16c9887794
sakshat193 <sakshat193@gmail.com>
2024-08-31 19:51:15 +0530

    Update models.py

--- models.py
+++ models.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.orm import sessionmaker, declarative_base
 import os
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = "sqlite:///Daam-dost-userData.db" 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
